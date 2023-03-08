92983e7408627dc2c4a8c248125b07c1b11610d3
Ninel <ninelgruzdeva@gmail.com>
2023-03-09 01:36:56 +0500

    fix: wrap app by redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Layout } from './components';
+import { setupStore } from './store';
 
-const App = () => <Layout />;
+const store = setupStore();
+
+const App = () => (
+  <Provider store={store}>
+    <Layout />
+  </Provider>
+);
 
 export default App;
