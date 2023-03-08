1a501a6b4b65c75db537b4bc3732cfb9d442b2b5
Ninel <ninelgruzdeva@gmail.com>
2023-03-08 18:02:27 +0500

    feat: create dummy data

---
+++ src/initial-data.js
@@ -0,0 +1,26 @@
+const initialData = {
+  tasks: [
+    {
+      id: 'task-1',
+      title: 'Take out the garbage',
+      description: 'Some random description',
+    },
+    {
+      id: 'task-2',
+      title: 'Watch my favorite show',
+      description: 'Some random description',
+    },
+    {
+      id: 'task-3',
+      title: 'Charge my phone',
+      description: 'Some random description',
+    },
+    {
+      id: 'task-4',
+      title: 'Cook dinner',
+      description: 'Some random description',
+    },
+  ],
+};
+
+export default initialData;
