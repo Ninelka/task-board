33080c232474033017132dede786eda1231cb24a
Ninel <ninelgruzdeva@gmail.com>
2023-03-10 20:14:24 +0500

    feat: add confirmation for editing and removing task

--- src/components/Modal/EditTaskModal.tsx
+++ src/components/Modal/EditTaskModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { v4 as uuidv4 } from 'uuid';
@@ -6,33 +6,84 @@ import Modal, { IModalProps } from './Modal';
 import styles from './Modal.module.css';
 import Button from '../Button/Button';
 import TaskForm from '../TaskForm/TaskForm';
-import { addNewTask, useAppDispatch } from '../../store';
-import { schema, FormData } from '../../lib/form-validation';
+import {
+  addNewTask,
+  updateTask,
+  useAppDispatch,
+  useAppSelector,
+} from '../../store';
+import { schema, FormData } from '../../lib';
+import ConfirmationModal from './ConfirmationModal';
+import { useModal } from '../../hooks/useModal';
 
-type EditTaskModalType = Omit<IModalProps, 'children'>;
+interface EditTaskModalType extends Omit<IModalProps, 'children'> {
+  taskId?: string;
+  withConfirmation?: boolean;
+}
 
 const EditTaskModal: React.FC<EditTaskModalType> = ({
+  taskId,
   isOpen,
   title,
+  withConfirmation,
   onClose,
 }) => {
-  const methods = useForm<FormData>({
-    resolver: yupResolver(schema),
-  });
+  const {
+    isConfirmationModalOpen,
+    openConfirmationModalHandler,
+    closeConfirmationModalHandler,
+  } = useModal();
+  const { tasks } = useAppSelector((state) => state.tasks);
   const dispatch = useAppDispatch();
 
-  const onSubmit = (data: FormData) => {
+  const currentTask = tasks.find((task) => task.id === taskId);
+
+  const methods = useForm<FormData>({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      id: currentTask?.id || '',
+      title: currentTask?.title || '',
+      description: currentTask?.description || '',
+    },
+  });
+
+  const onEdit = (data: FormData) => {
+    closeConfirmationModalHandler();
+
     dispatch(
-      addNewTask({
-        id: uuidv4(),
+      updateTask({
+        id: data.id!,
         title: data.title,
         description: data.description,
       })
     );
+
     if (onClose) {
       onClose();
     }
   };
+
+  const onSubmit = (data: FormData) => {
+    if (withConfirmation) {
+      openConfirmationModalHandler();
+    } else {
+      dispatch(
+        addNewTask({
+          id: uuidv4(),
+          title: data.title,
+          description: data.description,
+        })
+      );
+
+      if (onClose) {
+        onClose();
+      }
+    }
+  };
+
+  useEffect(() => {
+    methods.reset(currentTask);
+  }, [currentTask]);
 
   return (
     <Modal isOpen={isOpen} title={title} onClose={onClose}>
@@ -42,12 +93,21 @@ const EditTaskModal: React.FC<EditTaskModalType> = ({
         </div>
         <div className={styles.footer}>
           <Button
+            isDisabled={!methods.formState.isDirty}
             type="submit"
             className="btn btn-primary"
             onClick={methods.handleSubmit(onSubmit)}
           >
             Сохранить
           </Button>
+          {withConfirmation && (
+            <ConfirmationModal
+              onConfirm={methods.handleSubmit(onEdit)}
+              isOpen={isConfirmationModalOpen}
+              title="Сохранить изменения?"
+              onClose={closeConfirmationModalHandler}
+            />
+          )}
           <Button className="btn btn-secondary" onClick={onClose}>
             Закрыть
           </Button>
--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -2,15 +2,36 @@ import React from 'react';
 import styles from './TaskItem.module.css';
 import { Draggable } from 'react-beautiful-dnd';
 import { ITask } from '../../types';
+import Button from '../Button/Button';
+import { EditTaskModal } from '../index';
+import ConfirmationModal from '../Modal/ConfirmationModal';
+import { useModal } from '../../hooks/useModal';
+import { removeTask, useAppDispatch } from '../../store';
 
 interface ITaskItem extends Omit<ITask, 'index'> {
   id: string;
   title: string;
   description: string;
   index: number;
+  colId?: string;
 }
 
-const TaskItem: React.FC<ITaskItem> = ({ id, index, title, description }) => {
+const TaskItem: React.FC<ITaskItem> = ({
+  id,
+  index,
+  title,
+  description,
+  colId,
+}) => {
+  const {
+    isModalOpen,
+    isConfirmationModalOpen,
+    openModalHandler,
+    closeModalHandler,
+    openConfirmationModalHandler,
+    closeConfirmationModalHandler,
+  } = useModal();
+  const dispatch = useAppDispatch();
   return (
     <Draggable draggableId={id} index={index}>
       {(provided) => (
@@ -20,8 +41,48 @@ const TaskItem: React.FC<ITaskItem> = ({ id, index, title, description }) => {
           {...provided.dragHandleProps}
           className={styles.container}
         >
-          <h3 className={styles.title}>{title}</h3>
+          <header className={styles.header}>
+            <h5 className="m-0 text-truncate">{title}</h5>
+            {colId === 'ToDo' && (
+              <span className={styles.editBtn}>
+                <Button
+                  className="btn btn-sm"
+                  aria-label="Редактировать"
+                  onClick={openModalHandler}
+                >
+                  <i className="bi bi-pencil-square" />
+                </Button>
+                <EditTaskModal
+                  taskId={id}
+                  isOpen={isModalOpen}
+                  withConfirmation={true}
+                  onClose={closeModalHandler}
+                  title="Редактирование задачи"
+                />
+              </span>
+            )}
+          </header>
           <p className={styles.description}>{description}</p>
+          {colId === 'ToDo' && (
+            <span className={styles.removeBtn}>
+              <Button
+                className="btn btn-sm"
+                aria-label="Удалить"
+                onClick={openConfirmationModalHandler}
+              >
+                <i className="bi bi-trash text-danger" />
+              </Button>
+              <ConfirmationModal
+                isOpen={isConfirmationModalOpen}
+                onConfirm={() => {
+                  dispatch(removeTask({ colId: colId, taskId: id }));
+                  closeConfirmationModalHandler();
+                }}
+                onClose={closeConfirmationModalHandler}
+                title="Удалить данную задачу?"
+              />
+            </span>
+          )}
         </div>
       )}
     </Draggable>
