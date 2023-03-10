a2dad4d85083a1ae861a7bfe2c00182c861387b9
Ninel <ninelgruzdeva@gmail.com>
2023-03-10 15:09:04 +0500

    feat: add onConfirm handler to ConfirmationModal

--- src/components/Modal/ConfirmationModal.tsx
+++ src/components/Modal/ConfirmationModal.tsx
@@ -3,17 +3,22 @@ import Modal, { IModalProps } from './Modal';
 import styles from './Modal.module.css';
 import Button from '../Button/Button';
 
-const ConfirmationModal: React.FC<IModalProps> = ({
+interface ConfirmationModalType extends Omit<IModalProps, 'children'> {
+  onConfirm: () => void;
+}
+
+const ConfirmationModal: React.FC<ConfirmationModalType> = ({
   isOpen,
   title,
-  children,
+  onConfirm,
   onClose,
 }) => {
   return (
     <Modal isOpen={isOpen} title={title} onClose={onClose}>
-      <div className={styles.body}>{children}</div>
       <div className={styles.footer}>
-        <Button className="btn btn-primary">Ok</Button>
+        <Button className="btn btn-primary" onClick={onConfirm}>
+          Ok
+        </Button>
         <Button className="btn btn-secondary" onClick={onClose}>
           Отмена
         </Button>
