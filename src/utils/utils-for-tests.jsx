13cd7bef55bf27286c23e7b5eaebc9f17aa0237d
Ninel <ninelgruzdeva@gmail.com>
2023-03-11 20:26:29 +0500

    setup: app for testing

---
+++ src/utils/utils-for-tests.jsx
@@ -0,0 +1,26 @@
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import { configureStore } from '@reduxjs/toolkit';
+import TaskSlice from '../store/taskSlice';
+
+export function renderWithProviders(
+  ui,
+  {
+    preloadedState = {},
+    // Automatically create a store instance if no store was passed in
+    store = configureStore({
+      reducer: { tasks: TaskSlice.reducer },
+      preloadedState,
+    }),
+    ...renderOptions
+  } = {}
+) {
+  // eslint-disable-next-line react/prop-types
+  function Wrapper({ children }) {
+    // eslint-disable-next-line react/react-in-jsx-scope
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  // Return an object with the store and all of RTL's query functions
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
